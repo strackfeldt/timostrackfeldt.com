5f348fa1458daa953dadcee99950262f58e39203
Timo Strackfeldt <timo@strackfeldt.com>
2024-04-24 00:15:20 +0200

    feat: node ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://timostrackfeldt.com",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "static"
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
