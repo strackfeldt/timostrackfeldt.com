f61692d64bc9335d51f025b4732fcdabff009023
Timo Strackfeldt <timo@strackfeldt.com>
2025-10-31 10:19:19 +0100

    deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://timostrackfeldt.com",
   integrations: [mdx(), sitemap()],
@@ -13,4 +12,3 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 });
-
